f598e413765f8fa3de9de0f067b8ba3992569028
Mohammed Hussein <mohammed4shado@gmail.com>
2025-04-24 12:35:53 +0200

    Add files via upload

---
+++ DevFolio/DevFolio.js
@@ -0,0 +1,6 @@
+var typing = new Typed(".text", {
+    strings: ["Frontend Developer", "Graphic Designer", "CEO DevFolio", "Web Developer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
